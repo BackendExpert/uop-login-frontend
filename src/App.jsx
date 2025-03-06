5b2e8d0ecea6bd1b84cbba29fb5ef706c9d68be4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-06 14:09:44 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import StaffLogin from './pages/Auth/StaffLogin'
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer/Footer";
+import 'aos/dist/aos.css';
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import StaffLogin from "./pages/Auth/StaffLogin";
+import Nav from "./components/Nav/Nav";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 50) {
+
+      setShowNavBar(false);
+    } else {
+
+      setShowNavBar(true);
+    }
+    setIsTopOfPage(currentScrollY === 0);
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const shouldShowNavBar = !location.pathname.startsWith("/Dashboard");
+  const shouldShowFooter = !location.pathname.startsWith("/Dashboard");
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<StaffLogin /> } />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        {shouldShowNavBar && (
+          <div
+          className={`fixed top-0 w-full z-50 transition-transform duration-300 ${
+            showNavBar ? "translate-y-0" : "-translate-y-full"
+          } ${
+            isTopOfPage
+              ? "xl:mt-2"
+              : "shadow-md transition-colors duration-500"
+          }`}
+          >
+            <Nav />
+          </div>
+        )}
+        <Routes>
+          <Route path="*" element={<ErrorPage /> } />
+          <Route path="/" element={<StaffLogin /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   )
 }
-
-export default App
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus modi a rem! At hic sequi numquam soluta aspernatur ab, incidunt adipisci laudantium aliquam voluptatem necessitatibus, natus recusandae, omnis unde voluptatibus.
+    </div>
+  )
+}
+
+export default Nav
