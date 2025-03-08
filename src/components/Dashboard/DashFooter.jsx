a52d8439822d942624608f1b7699b3005eb86615
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-08 13:54:10 +0530

    Update

--- src/components/Dashboard/DashFooter.jsx
+++ src/components/Dashboard/DashFooter.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const DashFooter = () => {
+    const getcurrentyear = new Date().getFullYear()
   return (
-    <div>DashFooter</div>
+    <div className="py-8 text-[#a4805a]">
+        Copyright &copy; {getcurrentyear} MyHotel | All Right Reserved | Developed and Maintained by <a href="https://www.linkedin.com/in/jehanweerasuriya/" className='font-semibold text-[#4e5c4a]' target='_blank'>jehankandy</a>
+    </div>
   )
 }
 
--- src/pages/Auth/StaffLogin.jsx
+++ src/pages/Auth/StaffLogin.jsx
@@ -44,12 +44,12 @@ const StaffLogin = () => {
     }
     
   return (
-    <div className='mt-40 mx-28'>
+    <div className='mt-40 md:mx-28 mx-4'>
         <div className="">
             <h1 className="text-2xl font-semibold text-[#560606]">Staff Login</h1>
         </div>
 
-        <div className="w-1/2">
+        <div className="xl:w-1/2">
             <form onSubmit={headleSubmit} method="post">
                 <div className="my-2">
                     <h1 className="">Email</h1>
