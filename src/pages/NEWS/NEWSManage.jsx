0cce6ad234d5ecaa0dc5ed4411adb0011460f125
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-17 12:00:00 +0530

    Update NEWSManage.jsx

--- src/pages/NEWS/NEWSManage.jsx
+++ src/pages/NEWS/NEWSManage.jsx
@@ -7,65 +7,68 @@ const NEWSManage = () => {
 
     useEffect(() => {
         axios.get(import.meta.env.VITE_APP_API + '/news.php', {
-            params: { action: "getallNEWS" },  
+            params: { action: "getallNEWS" },
             headers: { 'Content-Type': 'multipart/form-data' },
         })
-        .then(res => {
-            console.log(res.data);
-            if (res.data.Result) {
-                setnewsdata(res.data.Result);
-            } else {
-                setnewsdata([]);  
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            setnewsdata([]); 
-        });
+            .then(res => {
+                console.log(res.data);
+                if (res.data.Result) {
+                    setnewsdata(res.data.Result);
+                } else {
+                    setnewsdata([]);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setnewsdata([]);
+            });
     }, []);
-    
 
 
-  return (
-    <div className='mt-4'>
-        <div className="flex">
-            <div className="">
-                <div className="inline-block p-2 bg-[#560606] rounded">
-                    <BsNewspaper className='h-6 w-auto fill-white'/>
+
+    return (
+        <div className='mt-4'>
+            <div className="flex">
+                <div className="">
+                    <div className="inline-block p-2 bg-[#560606] rounded">
+                        <BsNewspaper className='h-6 w-auto fill-white' />
+                    </div>
+                </div>
+                <div className="pl-4">
+                    <h1 className="text-[#560606] text-xl pt-1 font-semibold uppercase">NEWS Management</h1>
                 </div>
             </div>
-            <div className="pl-4">
-                <h1 className="text-[#560606] text-xl pt-1 font-semibold uppercase">NEWS Management</h1>
+
+            <div className="mt-4">
+                <a href="/Dashboard/CreateNEWS">
+                    <button className='bg-gradient-to-r from-[#ff7e60] to-[#ffc27c] px-8 py-2 text-white rounded duration-500'>
+                        Create New NEWS
+                    </button>
+                </a>
             </div>
-        </div>
-
-        <div className="mt-4">
-            <a href="/Dashboard/CreateNEWS">
-                <button className='bg-gradient-to-r from-[#ff7e60] to-[#ffc27c] px-8 py-2 text-white rounded duration-500'>
-                    Create New NEWS
-                </button>
-            </a>
-        </div>
 
 
-        <table className='w-full bg-white mt-4'>
-            <thead>
-                <tr className='h-12 w-full text-gray-500 border-b border-gray-200'>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Date</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-            {
+            <table className='w-full bg-white mt-4'>
+                <thead>
+                    <tr className='h-12 w-full text-gray-500 border-b border-gray-200'>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
                         Array.isArray(newsdata) && newsdata.length > 0 ? (
                             newsdata.map((news, index) => (
                                 <tr key={index} className='w-full h-16 text-center'>
-                                    <td>{index + 1}</td>    
+                                    <td>{index + 1}</td>
                                     <td>{news.news_title}</td>
                                     <td>{news.news_date}</td>
-                                    <td><a href={`/Dashboard/ViewNEWS/${news.id}`}><button className='text-[#560606] font-semibold'>Edit</button></a></td>
+                                    <td>
+                                        <a href={`/Dashboard/ViewNEWS/${news.id}`}><button className='mr-2 text-red-500 font-semibold'>Delete</button></a>
+                                        <a href={`/Dashboard/ViewNEWS/${news.id}`}><button className='text-[#560606] font-semibold'>Edit</button></a>
+                                    </td>
                                 </tr>
                             ))
                         ) : (
@@ -74,11 +77,11 @@ const NEWSManage = () => {
                             </tr>
                         )
                     }
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
 
-  )
+    )
 }
 
 export default NEWSManage
